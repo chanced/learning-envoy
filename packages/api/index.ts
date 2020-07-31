4c8e5d5d63f3cbd56c29478ee629d081cde4eb82
chanced <chanceusc@gmail.com>
2020-07-31 13:14:29 -0400

    cleanup

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -3,26 +3,24 @@ import Router from "@koa/router";
 import http from "http";
 
 const router = new Router();
+const appName = process.env.APP_NAME || "";
 
 router.get("/api", async (ctx, next) => {
   ctx.body = {
     route: "/api",
-    app: "api" + (process.env.APP_NAME || ""),
+    app: appName,
+  };
+  return next();
+});
+router.get("/api/test", async (ctx, next) => {
+  ctx.body = {
+    route: "/test",
+    app: appName,
   };
   return next();
 });
 
-router.get("/api/test", async (ctx, next) => {
-  ctx.body = {
-    route: "/test",
-    app: "api" + (process.env.APP_NAME || ""),
-  };
-});
-
 const app = new Koa();
-
 app.use(router.middleware());
-
 const server = http.createServer(app.callback());
-
 server.listen(7000, () => {});
